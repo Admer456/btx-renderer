5565271d22e515264a0e1853ab7f35f2f45f527d
Admer <49600504+Admer456@users.noreply.github.com>
2022-10-08 21:32:21 +0200

    RenderFrontend stub

--- renderer/RenderFrontend.cpp
+++ renderer/RenderFrontend.cpp
@@ -4,4 +4,128 @@
 #include "Precompiled.hpp"
 #include "RenderFrontend.hpp"
 
+static PluginRegistry Registry( EngineVersion );
 
+ADM_API PluginRegistry* GetPluginRegistry()
+{
+	Registry.Register<RenderFrontend>();
+	return &Registry;
+}
+
+bool RenderFrontend::Init( const EngineAPI& api )
+{
+	return true;
+}
+
+void RenderFrontend::Shutdown()
+{
+}
+
+Render::IBackend* RenderFrontend::GetBackend() const
+{
+	return nullptr;
+}
+
+void RenderFrontend::RenderView( const Render::IView* view )
+{
+}
+
+void RenderFrontend::DebugLine( adm::Vec3 start, adm::Vec3 end, adm::Vec3 colour, float life, bool depthTest )
+{
+}
+
+void RenderFrontend::DebugRay( adm::Vec3 start, adm::Vec3 direction, float length, bool withArrowhead, adm::Vec3 colour, float life, bool depthTest )
+{
+}
+
+void RenderFrontend::DebugBox( adm::Vec3 min, adm::Vec3 max, adm::Vec3 colour, float life, bool depthTest )
+{
+}
+
+void RenderFrontend::DebugCube( adm::Vec3 position, float extents, adm::Vec3 colour, float life, bool depthTest )
+{
+}
+
+void RenderFrontend::DebugSphere( adm::Vec3 position, float extents, adm::Vec3 colour, float life, bool depthTest )
+{
+}
+
+Render::IBatch* RenderFrontend::CreateBatch( const Render::BatchDesc& desc )
+{
+	return nullptr;
+}
+
+bool RenderFrontend::DestroyBatch( Render::IBatch* batch )
+{
+	return false;
+}
+
+const Vector<Render::IBatch*>& RenderFrontend::GetBatches() const
+{
+	// // O: insert return statement here
+	return {};
+}
+
+Render::IEntity* RenderFrontend::CreateEntity( const Render::EntityDesc& desc )
+{
+	return nullptr;
+}
+
+bool RenderFrontend::DestroyEntity( Render::IEntity* entity )
+{
+	return false;
+}
+
+const Vector<Render::IEntity*>& RenderFrontend::GetEntities() const
+{
+	// // O: insert return statement here
+	return {};
+}
+
+Render::ILight* RenderFrontend::CreateLight( const Render::LightDesc& desc )
+{
+	return nullptr;
+}
+
+bool RenderFrontend::DestroyLight( Render::ILight* light )
+{
+	return false;
+}
+
+const Vector<Render::ILight*>& RenderFrontend::GetLights() const
+{
+	// // O: insert return statement here
+	return {};
+}
+
+Render::IView* RenderFrontend::CreateView( const Render::ViewDesc& desc )
+{
+	return nullptr;
+}
+
+bool RenderFrontend::DestroyView( Render::IView* view )
+{
+	return false;
+}
+
+const Vector<Render::IView*>& RenderFrontend::GetViews() const
+{
+	// // O: insert return statement here
+	return {};
+}
+
+Render::IVolume* RenderFrontend::CreateVolume( const Render::VolumeDesc& desc )
+{
+	return nullptr;
+}
+
+bool RenderFrontend::DestroyVolume( Render::IVolume* volume )
+{
+	return false;
+}
+
+const Vector<Render::IVolume*>& RenderFrontend::GetVolumes() const
+{
+	// // O: insert return statement here
+	return {};
+}
--- renderer/RenderFrontend.hpp
+++ renderer/RenderFrontend.hpp
@@ -6,5 +6,41 @@
 class RenderFrontend : public IRenderFrontend
 {
 public:
+	bool Init( const EngineAPI& api ) override;
+	void Shutdown() override;
+
+	const char* GetPluginName() const override
+	{
+		return "BTX Test Renderer";
+	}
+
+	Render::IBackend* GetBackend() const override;
+
+	void RenderView( const Render::IView* view ) override;
 	
+	void DebugLine( adm::Vec3 start, adm::Vec3 end, adm::Vec3 colour, float life, bool depthTest ) override;
+	void DebugRay( adm::Vec3 start, adm::Vec3 direction, float length, bool withArrowhead, adm::Vec3 colour, float life, bool depthTest ) override;
+	void DebugBox( adm::Vec3 min, adm::Vec3 max, adm::Vec3 colour, float life, bool depthTest ) override;
+	void DebugCube( adm::Vec3 position, float extents, adm::Vec3 colour, float life, bool depthTest ) override;
+	void DebugSphere( adm::Vec3 position, float extents, adm::Vec3 colour, float life, bool depthTest ) override;
+
+	Render::IBatch* CreateBatch( const Render::BatchDesc& desc ) override;
+	bool DestroyBatch( Render::IBatch* batch ) override;
+	const Vector<Render::IBatch*>& GetBatches() const override;
+
+	Render::IEntity* CreateEntity( const Render::EntityDesc& desc ) override;
+	bool DestroyEntity( Render::IEntity* entity ) override;
+	const Vector<Render::IEntity*>& GetEntities() const override;
+
+	Render::ILight* CreateLight( const Render::LightDesc& desc ) override;
+	bool DestroyLight( Render::ILight* light ) override;
+	const Vector<Render::ILight*>& GetLights() const override;
+
+	Render::IView* CreateView( const Render::ViewDesc& desc ) override;
+	bool DestroyView( Render::IView* view ) override;
+	const Vector<Render::IView*>& GetViews() const override;
+
+	Render::IVolume* CreateVolume( const Render::VolumeDesc& desc ) override;
+	bool DestroyVolume( Render::IVolume* volume ) override;
+	const Vector<Render::IVolume*>& GetVolumes() const override;
 };
